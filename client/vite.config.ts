84866c4d7a1ed8a4d8e1996304412b9189a90955
zehan12 <zehan9211@gmail.com>
2025-08-28 01:42:43 +0530

    fix: :bug: add path alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
 })
