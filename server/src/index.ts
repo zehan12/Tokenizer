9170c5f2331164e221c037b82f9d1709aa18b34d
zehan12 <zehan9211@gmail.com>
2025-08-28 00:30:26 +0530

    feat: add health and alive route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,17 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/alive", () => ({
+    data: {
+      status: "Up"
+    }
+  }))
+  .get("/health", () => ({
+    data: {
+      status: "OK"
+    }
+  }))
+  .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
